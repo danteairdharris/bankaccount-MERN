905d385711fe82c4162f61a113da61687fba9b0f
Dante Aird-Harris <Dante>
2020-02-01 23:07:08 -0500

    added create account component in the frontend

--- src/components/new-account.component.js
+++ src/components/new-account.component.js
@@ -2,11 +2,70 @@ import React, { Component } from 'react';
 
 export default class NewAccount extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeBalance = this.onChangeBalance.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: "",
+            balance: ""
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onChangeBalance(e) {
+        this.setState({
+            balance: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const account = {
+            username: this.state.username,
+            balance: this.state.balance
+        }
+
+        console.log(account);
+        window.location = '/'
+    }
+
     render() {
         return(
             <div>
-                <p>You are on the new account component!</p>
-            </div>
+              <h3> Create New Account</h3>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                    <label>Username: </label>
+                    <input type="text"
+                    required
+                    className="form-control"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}/>
+                </div>
+                <div className="form-group">
+                    <label>Balance: </label>
+                    <input type="text"
+                    required
+                    className="form-control"
+                    value={this.state.balance}
+                    onChange={this.onChangeBalance}/>
+                </div>
+                <div className="form-group">
+                    <input type="submit"
+                    value="Create Account"
+                    className="btn btn-primary"/>
+                </div>
+              </form>
+             </div>
         )
     }
 }
